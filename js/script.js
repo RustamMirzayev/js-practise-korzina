93def37c319ac7b220af67b109e2b0d6dec8e4d8
RustamMirzayev <mirzayevrustam863@gmail.com>
2024-06-26 11:17:18 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+// window.addEventListener("DOMContentLoaded", () => {
+//   let products = document.querySelectorAll(".product"),
+//     buttons = document.querySelectorAll("button"),
+//     openBtn = document.querySelector(".open");
+
+//   let cart = document.createElement("div"),
+//     field = document.createElement("div"),
+//     heading = document.createElement("h2"),
+//     closeBtn = document.createElement("button");
+
+//   function createCart() {
+//     cart.classList.add("cart");
+//     field.classList.add("cart-field"), closeBtn.classList.add("close");
+
+//     heading.textContent = "в нашей корзине";
+//     closeBtn.textContent = "закрит";
+
+//     document.body.appendChild(cart);
+//     cart.appendChild(heading);
+//     cart.appendChild(field);
+//     cart.appendChild(closeBtn);
+//   }
+//   createCart();
+
+//   openBtn.addEventListener("click", () => {
+//     cart.style.display = "block";
+//   });
+
+//   closeBtn.addEventListener("click", () => {
+//     cart.style.display = "none";
+//   });
+
+//   buttons.forEach((item, i) => {
+//     item.addEventListener("click", () => {
+//       let cloneItem = products[i].cloneNode(true),
+//         btn = cloneItem.querySelector("button");
+
+//       btn.remove();
+//       field.appendChild(cloneItem);
+//       products[i].remove();
+//     });
+//   });
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  let openBtn = document.querySelector(".open"),
+    products = document.querySelectorAll(".product"),
+    buttons = document.querySelectorAll("button");
+
+  let cart = document.createElement("div"),
+    field = document.createElement("div"),
+    heading = document.createElement("h2"),
+    closeBtn = document.createElement("button");
+
+  function createCart() {
+    cart.classList.add("cart");
+    field.classList.add("cart-field");
+    closeBtn.classList.add("close");
+
+    heading.textContent = "в нашей корзине";
+    closeBtn.textContent = "закрит";
+
+    document.body.appendChild(cart);
+    cart.appendChild(heading);
+    cart.appendChild(field);
+    cart.appendChild(closeBtn);
+  }
+  createCart();
+
+  openBtn.addEventListener("click", () => {
+    cart.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    cart.style.display = "none";
+  });
+
+  buttons.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      let cloneItem = products[i].cloneNode(true),
+        btn = cloneItem.querySelector("button");
+
+      btn.remove();
+      field.appendChild(cloneItem);
+      products[i].remove();
+    });
+  });
+});
